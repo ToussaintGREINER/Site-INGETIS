238eb66f3848d1264058d7743c7be078e1a06a01
ToussaintGREINER <145444632+ToussaintGREINER@users.noreply.github.com>
2023-11-07 11:26:24 +0100

    Add files via upload

---
+++ mission_1_js/main.js
@@ -0,0 +1,12 @@
+let q1_answer = 2;
+let q1_selected;
+
+
+
+function radioCheck1() {
+	q1_selected = document.getElementById("an1").checked
+	var txt = "rien"
+	if(q1_selected = true) {
+		txt = "good"
+	}
+}
